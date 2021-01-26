323cc7407be2f786cb5cbc9eddc0985d98f91a8d
Mdex <76643605+mdexSwap@users.noreply.github.com>
2021-01-26 19:55:53 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/mdexSwap/info@main/precache-manifest.25c632f14406820aaaa7232f4423f27f.js"
+  "https://cdn.jsdelivr.net/gh/mdexSwap/info@main/precache-manifest.360d4d8cb2abc1130795fb5702c700b9.js"
 );
 
 self.addEventListener('message', (event) => {
