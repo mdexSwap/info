68eefbb297ca0090e0a2b1126d2e9b2837eaf0eb
mdexswap <mdexswap@protonmail.com>
2021-06-25 01:15:14 +0800

    update

---
+++ precache-manifest.41e0b635428b1903f96528f391dd208d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8ac8869096a69bd19cfb6bbf9e9f64",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/info@main/index.html"
+  },
+  {
+    "revision": "a77e50ee388a11a00d2a",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/info@main/static/js/2.a7ab006c.chunk.js"
+  },
+  {
+    "revision": "ccb1cc9b80462227be6a47ac15ed2782",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/info@main/static/js/2.a7ab006c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72498d106df6f378c6cc",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/info@main/static/js/main.c38c28db.chunk.js"
+  },
+  {
+    "revision": "394ca2a8607839d02804",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/info@main/static/js/runtime-main.3ca09b36.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/info@main/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "8e72f6f2edbc451e3bba41e50d2b8fcd",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/info@main/static/media/logo.8e72f6f2.png"
+  }
+]);
